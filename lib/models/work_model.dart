74fe2b09cf0e7132c5218e7ee3e5b68408f7c957
MohamedANaagi <nmhmd7602@gmail.com>
2025-12-14 21:22:51 +0300

    Reorder and reintroduce Meals App entry in WorkModel for consistency

--- lib/models/work_model.dart
+++ lib/models/work_model.dart
@@ -19,17 +19,6 @@ class WorkModel {
 
   static const List<WorkModel> defaultWorks = [
     WorkModel(
-      id: '1',
-      title: "Meals App",
-      date: "2025",
-      subtitle: "Flutter • Recipes & Meal Management",
-      description:
-          "A fully responsive Flutter meals app using flutter_screenutil and google_fonts. Features include go_router navigation, shared_preferences for user settings, and sqflite with path for local recipe storage. The app also integrates onboarding flows and interactive UI elements using dots_indicator and carousel_slider.",
-      imageUrl: "assets/images/1.jpeg",
-      url:
-          "https://drive.google.com/drive/folders/1H4aJX7etf0dlc6Cpc_3IxFgcf70akAjn?usp=drive_link",
-    ),
-    WorkModel(
       id: '3',
       title: "Todo App — React Native & Convex",
       date: "2025",
@@ -41,6 +30,18 @@ class WorkModel {
       url:
           "https://drive.google.com/drive/folders/142mjegxjS1SIkmsgXMB8QBgMCM8Rtt-9?usp=drive_link", // يمكنك إضافة الرابط هنا
     ),
+    WorkModel(
+      id: '1',
+      title: "Meals App",
+      date: "2025",
+      subtitle: "Flutter • Recipes & Meal Management",
+      description:
+          "A fully responsive Flutter meals app using flutter_screenutil and google_fonts. Features include go_router navigation, shared_preferences for user settings, and sqflite with path for local recipe storage. The app also integrates onboarding flows and interactive UI elements using dots_indicator and carousel_slider.",
+      imageUrl: "assets/images/1.jpeg",
+      url:
+          "https://drive.google.com/drive/folders/1H4aJX7etf0dlc6Cpc_3IxFgcf70akAjn?usp=drive_link",
+    ),
+
     WorkModel(
       id: '2',
       title: "Mobile App Redesign",
